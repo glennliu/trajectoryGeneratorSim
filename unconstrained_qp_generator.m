61d4cbc20b94575f840a3bdb8ad03398fe7bf366
Chuhao Liu <chuhao.liu@gmail.com>
2018-12-04 12:05:15 +0800

    test multi segs

--- unconstrained_qp_generator.m
+++ unconstrained_qp_generator.m
@@ -6,17 +6,12 @@ clear all;
     kr = 4; % derivative order
     frame = 100;    % number of frames
     num_st = 4; % number of constrained states
-    
-    % derivative constraint
-    % [x0;xt;vx0;vxt;ax0;axt;w0;wt];
-%     segments0 = [0 4 0 0 0 0 0 0 
-%                 4 11 0 0 0 0 0 0 
-%                 11 17 0 0 0 0 0 0
-%                 ];
             
     % [states_start states_end]
     segments0 = [0 0 0 0 4 3 0 0;
-                  4 3 0 0 17 0 0 0];
+                  4 3 0 0 11 0 0 0
+                  11 0 0 0 22 0 0 0
+                  ];
             
     num_seg = size(segments0,1);
     dim = size(segments0,2)/(num_st*2);
@@ -42,13 +37,7 @@ clear all;
         end
     end
     
-    %% mellinger method
-    Ad1 = Ad0;
-    Q1 = kron(eye(num_seg),Q0);
-    poly1 = poly0;
-    segments1 = zeros(1,num_st*2*dim*num_seg);
-    path1 = zeros(frame*num_seg+1,2);
-
+    %find constraint matrix Ad
     for is = 1:num_seg
         for k=0:3
             for i=k:N
@@ -56,55 +45,72 @@ clear all;
                 Ad1(1+k +(is-0.5)*2*kr,i+1+(is-1)*(N+1)) = (factorial(i)/factorial(i-k))*ts(is+1)^(i-k);    % end states
             end
         end
-                segments1(1,(N+1)*(is-1)+1:(N+1)*is) = segments0(is,1:end);  % just for x now
-
+        segments1(1,(N+1)*(is-1)+1:(N+1)*is) = segments0(is,1:end);  % just for x now
     end
     
-%     for ip = 1:num_seg
+    %% mellinger method
+%     Ad1 = Ad0; 
+%     Q1 = kron(eye(num_seg),Q0); 
+%     poly1 = poly0; segments1 =
+%     zeros(1,num_st*2*dim*num_seg); path1 = zeros(frame*num_seg+1,2);
+% 
+%     for is = 1:num_seg
+%         for k=0:3
+%             for i=k:N
+%                 Ad1(1+k +(is-1)*2*kr,i+1+(is-1)*(N+1)) =
+%                 (factorial(i)/factorial(i-k))*ts(is)^(i-k);    % start
+%                 states Ad1(1+k +(is-0.5)*2*kr,i+1+(is-1)*(N+1)) =
+%                 (factorial(i)/factorial(i-k))*ts(is+1)^(i-k);    % end
+%                 states
+%             end
+%         end
+%                 segments1(1,(N+1)*(is-1)+1:(N+1)*is) =
+%                 segments0(is,1:end);  % just for x now
+% 
 %     end
-    poly1 = quadprog(Q1,f,[],[],Ad1,segments1');
-    
-    
-    for is = 1:num_seg
-        for index=1+(is-1)*frame:frame+(is-1)*frame
-            t = ts(is) +(index-1-(is-1)*frame)*T/frame;
-            for i=0:N   % x path
-                path1(index,1) = t;
-                path1(index,2) = poly1(i+1+(is-1)* (N+1))*t^(i) + path1(index,2);  %x
-            end
-        end
-    end
-    path1(frame*num_seg+1,:) = [t+T/frame segments1(1,end-num_st+1)];
-    
+%     
+%     poly1 = quadprog(Q1,f,[],[],Ad1,segments1');
+%     
+%     for is = 1:num_seg
+%         for index=1+(is-1)*frame:frame+(is-1)*frame
+%             t = ts(is) +(index-1-(is-1)*frame)*T/frame; for i=0:N   % x
+%             path
+%                 path1(index,1) = t; path1(index,2) = poly1(i+1+(is-1)*
+%                 (N+1))*t^(i) + path1(index,2);  %x
+%             end
+%         end
+%     end path1(frame*num_seg+1,:) = [t+T/frame segments1(1,end-num_st+1)];
     
     %% unconstained method
-    Q2 = Q1;
+    Q2 = kron(eye(num_seg),Q0); 
     Ad2 = Ad1;
     segments2 = segments1;
     path2 = zeros(frame*num_seg+1,2);
     
     % find d
-    df = [0 0 0 0 11 0 0 0]';
+    df = [0 0 0 0 22 0 0 0]';
     
     % selection matrix C
     Ct = zeros(num_seg*2*kr,(num_seg+1)*kr);
-%     Ct(1,1) = 1;
-%     Ct(13,2) = 1;
     Ct(1:4,1:4) = eye(4);
-    Ct(5:8,9:12) = eye(4);
-    Ct(9:12,9:12) = eye(4);
-    Ct(13:16,5:8) =eye(4);
+    Ct(end-3:end,5:8) = eye(4);
+    
+    Ct_0 = [eye(4);eye(4)];
+    Ct(5:end-4,9:end) = kron(eye(2),Ct_0);
+    
+%     Ct(13:16,5:8) =eye(4);
     
     % find R
     R = Ct.'*inv(Ad2).'*Q2*inv(Ad2)*Ct;
     R_pp = R(9:end,9:end);
     R_fp = R(1:8,9:end);
 
+
     % solver
     dp_star = -inv(R_pp)*R_fp'*df;
     dp = conj(dp_star);
     d_st = Ct *[df;dp];
-    poly2 = inv(Ad2) * segments2';
+    poly2 = inv(Ad2) * d_st;
     
     % calculate trajectory
     for is = 1:num_seg
@@ -120,8 +126,8 @@ clear all;
     
     %% plot tools
     
-    plot(path1(:,1),path1(:,2));
-    hold on;
+%     plot(path1(:,1),path1(:,2));
+%     hold on;
     plot(path2(:,1),path2(:,2));
     grid on;
 
