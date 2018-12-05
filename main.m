8dcb17ebc4f86a415f756bc006add4e0e9bde18b
Chuhao Liu <chuhao.liu@gmail.com>
2018-12-05 15:41:30 +0800

    test for generation

--- main.m
+++ main.m
@@ -1,12 +1,14 @@
 clear all;
 %% set paramters
 
-segments_3d = [0 0 0 0 2 0 0 0 1.2 0 0 0
-               0 0 0 0 2 0 0 0 1.2 0 0 0 
+segments_3d = [-2 0 0 0 0 0 0 0 1.2 0 0 0
+               2 0 0 0 0 0 0 0 1.2 0 0 0 
                ];
           
-transit_pose_3d = [4 8 1.2
-                    8 3 1.2
+transit_pose_3d = [-1.6 0.5 1.2
+                    -0.3 0.5 1.2
+                    0.3 -0.5 1.2
+                    1.6 -0.8 1.2
     ];
            
 params.t0 = 0;
@@ -17,7 +19,6 @@ params.num_st = 4; % number of constrained states
 
 %% generate trajectory
 path = create_3d_qp_trajectory(segments_3d,transit_pose_3d,params);
-
 
 %% plot
 run_trajectory(path);
