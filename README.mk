4b11e3e9d24d853250db0cdff9c509ba20729826
Chuhao Liu <chuhao.liu@gmail.com>
2018-12-12 15:15:39 +0800

    test

--- README.mk
+++ README.mk
@@ -1,4 +1,7 @@
 Get Start
 
 ![alt text](https://github.com/glennliu/trajectoryGeneratorSim/blob/master/imgs/interactive_marker.png)
+![alt text](~/imgs/interactive_marker.png)
 
+![alt text](./imgs/interactive_marker.png)
+![alt text](imgs/interactive_marker.png)
